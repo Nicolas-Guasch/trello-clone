c8db5da9abce8f287f57650bb706a73c8c647f52
Nicolas Guasch <nic.guasch@gmail.com>
2024-10-15 17:05:55 -0300

    Improve login form

--- src/app/domains/login/pages/login/login.component.ts
+++ src/app/domains/login/pages/login/login.component.ts
@@ -4,8 +4,13 @@ import { Component } from '@angular/core';
   selector: 'app-login',
   standalone: true,
   imports: [],
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
 })
 export class LoginComponent {
-
+  socialButtons = [
+    { label: 'Google', image: '/images/logo/google-logo.5867462c.svg' },
+    { label: 'Microsoft', image: '/images/logo/microsoft-logo.c73d8dca.svg' },
+    { label: 'Apple', image: '/images/logo/apple-logo.54e0d711.svg' },
+    { label: 'Slack', image: '/images/logo/slack-logo.5d730c10.svg' },
+  ];
 }
